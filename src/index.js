5fd7240686f56816a805d2b8e501950267718059
Jerry Tarus <jerrytarus@gmail.com>
2023-10-19 15:52:25 +0300

    Added event listeners, a few functions and fetch request

--- src/index.js
+++ src/index.js
@@ -60,4 +60,52 @@ function toggleFilterDogs(e) {
     dogInfo.append(dogImg, dogTitle, dogButton);
   }
 
+
+  function onGoodDogButtonClick(e) {
+    let newValue;
+    if (e.target.innerText.includes("Good")) {
+      e.target.innerText = "Bad Dog";
+      newValue = false;
+    } else {
+      e.target.innerText = "Good Dog";
+      newValue = true;
+    }
+    toggleGoodDog(e.target.dataset.id, newValue).then(updateDogBar);
+  }
+  
+  function updateDogBar() {
+    const filterDogsButton = document.querySelector("#good-dog-filter");
+    if (filterDogsButton.innerText.includes("OFF")) {
+      getDogs().then((dogArray) => addAllDogsToDogBar(dogArray));
+    } else {
+      getDogs().then((dogArray) => addAllDogsToDogBar(dogArray, true));
+    }
+  }
+  
+  // fetches:
+  
+  const baseURL = "http://localhost:3000/pups";
+  
+  function getDogs() {
+    return fetch(baseURL).then((r) => r.json());
+  }
+  
+  function getSingleDog(id) {
+    return fetch(baseURL + `/${id}`).then((r) => r.json());
+  }
+  
+  function toggleGoodDog(id, newValue) {
+    const options = {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        isGoodDog: newValue,
+      }),
+    };
+    return fetch(baseURL + `/${id}`, options).then((r) => r.json());
+  }
+  
+
   
