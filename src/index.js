59bf18fe74da208239c2c1b9fd9fc03a63d0566a
Jerry Tarus <jerrytarus@gmail.com>
2023-10-19 15:51:45 +0300

    Added a few other functions and event listeners

--- src/index.js
+++ src/index.js
@@ -27,4 +27,37 @@ function toggleFilterDogs(e) {
     }
   }
 
+  function addDogSpantoDogBar(dog) {
+    const dogBar = document.querySelector("#dog-bar");
+    const dogSpan = document.createElement("span");
+    dogSpan.innerText = dog.name;
+    dogSpan.dataset.id = dog.id;
+  
+    dogSpan.addEventListener("click", onDogSpanClick);
+  
+    dogBar.append(dogSpan);
+  }
+  
+  function onDogSpanClick(e) {
+    getSingleDog(e.target.dataset.id).then(addDogInfoToPage);
+  }
+  
+
+  function addDogInfoToPage(dog) {
+    const dogInfo = document.querySelector("#dog-info");
+    dogInfo.innerHTML = "";
+    const dogImg = document.createElement("img");
+    dogImg.src = dog.image;
+  
+    const dogTitle = document.createElement("h2");
+    dogTitle.innerText = dog.name;
+  
+    const dogButton = document.createElement("button");
+    dogButton.innerText = dog.isGoodDog ? "Good Dog!" : "Bad Dog!";
+    dogButton.dataset.id = dog.id;
+    dogButton.addEventListener("click", onGoodDogButtonClick);
+  
+    dogInfo.append(dogImg, dogTitle, dogButton);
+  }
+
   
